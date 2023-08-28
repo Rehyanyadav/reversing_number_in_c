1943f27f2bb5a18eacf72f18c8c6838ba7dca0a2
root:$rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2023-08-28 20:29:34 +0530

    reversing_number_in_c_uploading
    reversing the number in c using while loop  by rehyan yadav

---
+++ Q9.c
@@ -0,0 +1,18 @@
+
+// write a program in c to  take input  and reverse using while loop 
+
+#include<stdio.h>  
+ int main()    
+{    
+int n, reverse=0, rem;    
+printf("Enter a number: ");    
+  scanf("%d", &n);    
+  while(n!=0)    
+  {    
+     rem=n%10;    
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+  printf("Reversed Number: %d",reverse);    
+return 0;  
+}  
